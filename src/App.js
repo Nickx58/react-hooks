a509207f723d12e704d8785a43a5cd78898e3d34
Nickx58 <nikhilx58@gmail.com>
2021-01-06 11:24:06 +0530

    Build Search Bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
-    {
-        title: "React",
-        content: "React is a famouse JS libraray"
-    },
-    {
-        title: "Angular",
-        content: "Angular is a JS framework"
-    },
-    {   
-        title: "Vue",
-        content: "Worse framework among others"
-    }
+	{
+		title: "React",
+		content: "React is a famouse JS libraray"
+	},
+	{
+		title: "Angular",
+		content: "Angular is a JS framework"
+	},
+	{
+		title: "Vue",
+		content: "Worse framework among others"
+	}
 ];
 
 const App = () => {
-    return (
-        <div><Accordion items={items}/></div>
-    )
+	return (
+		<div>
+			<Search />
+			<Accordion items={items} />
+		</div>
+	)
 }
 
 export default App;
