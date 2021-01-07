533c5f8e26489df43462aa9be44ff1499d45a081
Nickx58 <nikhilx58@gmail.com>
2021-01-07 16:43:12 +0530

    Filtering list

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import DropDown from "./components/DropDown";
 
 const items = [
-	{
-		title: "React",
-		content: "React is a famouse JS libraray"
-	},
-	{
-		title: "Angular",
-		content: "Angular is a JS framework"
-	},
-	{
-		title: "Vue",
-		content: "Worse framework among others"
-	}
+  {
+    title: "React",
+    content: "React is a famouse JS libraray",
+  },
+  {
+    title: "Angular",
+    content: "Angular is a JS framework",
+  },
+  {
+    title: "Vue",
+    content: "Worse framework among others",
+  },
 ];
 
 const options = [
-	{
-		label: "The color of red",
-		value: "red"
-	},
-	{
-		label: "Beauty in black",
-		value: "black"
-	},
-	{
-		label: "Olive green is fav",
-		value: "oliveGreen"
-	}
+  {
+    label: "The color of red",
+    value: "red",
+  },
+  {
+    label: "Beauty in black",
+    value: "black",
+  },
+  {
+    label: "Olive green is fav",
+    value: "oliveGreen",
+  },
 ];
 
 const App = () => {
-	return (
-		<div>
-			{/* <Search />
+  const [selected, setSelected] = useState(options[0]);
+  return (
+    <div>
+      {/* <Search />
 			<Accordion items={items} /> */}
-			<DropDown options={options} />
-		</div>
-	)
-}
+      <DropDown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const DropDown = ({ options }) => {
+const DropDown = ({ options, selected, onSelectedChange }) => {
   const renderOptions = options.map((option) => {
+    if(option.value === selected.value) {
+        return null;
+    }
     return (
-      <div key={option.value} className="item">
+      <div onClick={() => onSelectedChange(option)} key={option.value} className="item">
         {option.label}
       </div>
     );
@@ -14,7 +17,7 @@ const DropDown = ({ options }) => {
         <label className="label">Select a color</label>
         <div className="ui selection dropdown visible active">
           <i className="dropdown icon"></i>
-          <div className="text">Select color</div>
+          <div className="text">{selected.label}</div>
           <div className="menu visible transition">{renderOptions}</div>
         </div>
       </div>
